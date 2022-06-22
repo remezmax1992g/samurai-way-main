e3f75de13c4e6455e1eaa0ac78d08f4940b2094a
Maxim Remez <remezmaxim1992@gmail.com>
2022-06-22 14:51:27 +0300

    Lesson 12 React

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhnDz08lUWsDll_7m1NYkHV_U2gNmfvTWCGg&usqp=CAU"/>
+        </header>
+    );
+};
+
+export default Header;
