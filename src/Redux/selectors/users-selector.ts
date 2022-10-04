37c11d84da74b46964cf5ed59ad58eb621dcabf6
Maxim Remez <remezmaxim1992@gmail.com>
2022-10-05 00:35:01 +0300

    82-83 - reselect

--- src/Redux/selectors/users-selector.ts
+++ src/Redux/selectors/users-selector.ts
@@ -1,7 +1,8 @@
 import {RootStateType} from "../redux-store";
 import {UserType} from "../reducers/users-reducer";
-
-export const getUsers = (state: RootStateType): Array<UserType> => {return state.userPage.items}
+import {createSelector} from "reselect";
+const getUsersSelector = (state: RootStateType): Array<UserType> => {return state.userPage.items}
+export const getUsers = createSelector(getUsersSelector, (users: Array<UserType>) => { return users})
 export const getPageSize = (state: RootStateType): number => {return state.userPage.pageSize}
 export const getTotalUsersCount = (state: RootStateType): number => {return state.userPage.totalUsersCount}
 export const getCurrentPage = (state: RootStateType): number => {return state.userPage.currentPage}
