342a073ef27e87f8e18d5bf6b67cc0e6de4dbb55
Maxim Remez <remezmaxim1992@gmail.com>
2022-10-06 10:26:08 +0300

    90 - refactoring of login

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -4,7 +4,10 @@ import {createLogin} from "../../Redux/reducers/auth-reducer";
 import {useFormik} from "formik";
 import {Redirect} from "react-router-dom";
 import {RootStateType} from "../../Redux/redux-store";
-
+type LoginFieldsType = {
+    email?: string
+    password?: string
+}
 const Login = () => {
     const dispatch = useDispatch()
     const isLogin = useSelector<RootStateType, boolean>(state => state.auth.isAuth)
@@ -18,48 +21,68 @@ const Login = () => {
             dispatch(createLogin(values))
         },
         validate: values => {
+            const errors: LoginFieldsType = {}
             if (!values.email) {
-                return  {email: "Email is required"}
+                errors.email = 'Email is required'
             }
             if (!values.password) {
-               return {password: "Password is required"}
+                errors.password = 'Password is required'
             }
+            return errors
         }
     })
     if (isLogin) {
         return <Redirect to={"/Profile"}/>
     }
-    return( <form onSubmit={formik.handleSubmit}>
-            <div>
-                <input type="email"
-                       name="email"
-                       placeholder="Email"
-                       onChange={formik.handleChange}
-                       value={formik.values.email}
-                />
-            </div>
-            {formik.errors.email ? <div>{formik.errors.email}</div> : null}
-            <div>
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                />
-            </div>
-            {formik.errors.password ? <div>{formik.errors.password}</div> : null}
-            <div>
-                <input
-                    type="checkbox"
-                    name="rememberMe"
-                    checked={formik.values.rememberMe}
-                    onChange={formik.handleChange}
-                />
-                <span>Remember me</span>
-            </div>
-            <button type="submit">Login</button>
-        </form>
+    return (
+        <div>
+            <label>
+                <p>
+                    To log in get registered <a href="https://social-network.samuraijs.com/"
+                                                target={"_blank"}>here</a>
+                </p>
+                <p>
+                    or use common test account credentials:
+                </p>
+                <p>
+                    Email: free@samuraijs.com
+                </p>
+                <p>
+                    Password: free
+                </p>
+            </label>
+            <form onSubmit={formik.handleSubmit}>
+                <div>
+                    <input type="email"
+                           name="email"
+                           placeholder="Email"
+                           onChange={formik.handleChange}
+                           value={formik.values.email}
+                    />
+                </div>
+                {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                <div>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                    />
+                </div>
+                {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+                <div>
+                    <input
+                        type="checkbox"
+                        name="rememberMe"
+                        checked={formik.values.rememberMe}
+                        onChange={formik.handleChange}
+                    />
+                    <span>Remember me</span>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
     )
 }
 
