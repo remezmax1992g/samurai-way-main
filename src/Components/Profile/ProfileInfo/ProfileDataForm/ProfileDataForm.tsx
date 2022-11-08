1e6e64b74c9c0eeebdd0ee89d45ad618ef1522cd
Maxim Remez <remezmaxim1992@gmail.com>
2022-11-08 19:31:58 +0300

    97 - add adjustment of contacts in FormProfileData

--- src/Components/Profile/ProfileInfo/ProfileDataForm/ProfileDataForm.tsx
+++ src/Components/Profile/ProfileInfo/ProfileDataForm/ProfileDataForm.tsx
@@ -3,6 +3,8 @@ import {ProfileType, saveProfileTC} from "../../../../Redux/reducers/profile-red
 import {useDispatch, useSelector} from "react-redux";
 import {useFormik} from "formik";
 import {RootStateType} from "../../../../Redux/redux-store";
+import Contact from "../ProfileData/Contact/Contact";
+import {ContactsType} from "../../../../api/api";
 
 type ProfileDataForm = {
     setEditMode: () => void
@@ -11,6 +13,7 @@ type ProfileDataForm = {
 const ProfileDataForm = ({setEditMode}: ProfileDataForm) => {
     const dispatch = useDispatch()
     const profile = useSelector<RootStateType, ProfileType>(state => state.profilePage.profile)
+    console.log(profile.contacts["facebook"])
     const formik = useFormik({
         initialValues: {
             userId: profile.userId,
@@ -40,33 +43,62 @@ const ProfileDataForm = ({setEditMode}: ProfileDataForm) => {
                 <button type="submit">SAVE</button>
             </div>
             <div>
-                <b>Full name:</b><input type="text"
-                                        name="fullName"
-                                        placeholder="Enter fullName"
-                                        onChange={formik.handleChange}
-                                        value={formik.values.fullName}
-            />
+                <b>Full name:</b>
+                <div>
+                    <input type="text"
+                           name="fullName"
+                           placeholder="Enter fullName"
+                           onChange={formik.handleChange}
+                           value={formik.values.fullName}
+                    />
+                </div>
             </div>
             <div>
-                <b>Looking for a job:</b><input type="checkbox"
-                                                name="lookingForAJob"
-                                                onChange={formik.handleChange}
-                                                checked={formik.values.lookingForAJob}
-            />
+                <b>Looking for a job:</b>
+                <div>
+                    <input type="checkbox"
+                           name="lookingForAJob"
+                           onChange={formik.handleChange}
+                           checked={formik.values.lookingForAJob}
+                    />
+                </div>
             </div>
             <div>
-                <b>My professional skills:</b><textarea name="lookingForAJobDescription"
-                                                        placeholder="Enter professional skills"
-                                                        onChange={formik.handleChange}
-                                                        value={formik.values.lookingForAJobDescription}
-            />
+                <b>My professional skills:</b>
+                <div>
+                    <textarea name="lookingForAJobDescription"
+                              placeholder="Enter professional skills"
+                              onChange={formik.handleChange}
+                              value={formik.values.lookingForAJobDescription}
+                    />
+                </div>
             </div>
             <div>
-                <b>My personal information:</b><textarea name="aboutMe"
-                                                        placeholder="Enter personal information"
-                                                        onChange={formik.handleChange}
-                                                        value={formik.values.aboutMe}
-            />
+                <b>My personal information:</b>
+                <div>
+                    <textarea name="aboutMe"
+                              placeholder="Enter personal information"
+                              onChange={formik.handleChange}
+                              value={formik.values.aboutMe}
+                    />
+                </div>
+            </div>
+            <div>
+                <b>Contacts:</b>{Object.keys(profile.contacts).map((key) => {
+                return (
+                    <div>
+                        <b>{key}:</b>
+                        <div>
+                            <input type="text"
+                                   name={"contacts." + key}
+                                   placeholder={key}
+                                   onChange={formik.handleChange}
+                                   value={formik.values.contacts[key as keyof ContactsType]}
+                                   />
+                        </div>
+                    </div>
+                )
+            })}
             </div>
         </form>
     );
